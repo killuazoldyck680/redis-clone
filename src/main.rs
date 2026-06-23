ce781dca468b1de476a3ee0cb0e0403fd244bfa0
killuazoldyck680 <killuaz2049@gmail.com>
2026-06-23 18:34:32 +0530

    step -1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+
+use std::io::{Write,Read};
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(_stream) => {
+                println!("accepted new connection")
+            }
+
+            Ok(mut stream) => {
+                stream.write_all(b"+PONG\r\n").unwrap();
+            }
+
+            Err(e) => {
+                println!("error occured {}",e)
+            }
+        }
+    }
 }
