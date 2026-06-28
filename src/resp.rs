ad7b943497ce36687a33e90e5adee2ebdbe6ec56
killuazoldyck680 <killuaz2049@gmail.com>
2026-06-28 19:09:41 +0530

    add fn serialize

--- src/resp.rs
+++ src/resp.rs
@@ -1,5 +1,5 @@
-use std::net::TcpStream;
-use bytes::{BytesMut};
+use tokio::{net::TcpStream, io::{AsyncReadExt, AsyncWriteExt}};
+use bytes::BytesMut;
 
 
 
@@ -16,7 +16,15 @@ pub struct RespHandler {
     
 }
 
-impl Value
+impl Value {
+    pub fn serialize(self) -> String {
+        match self {
+            Value::SimpleString(s) => format!("+{}\r\n", s),
+            Value::BulkString(s) => format!("${}\r\n{}\r\n", s.chars().count(), s),
+            _ => panic!("Unsupported value for serialize")
+        }
+    }
+}
 
 impl RespHandler {
     pub fn new(stream:TcpStream) -> Self {
