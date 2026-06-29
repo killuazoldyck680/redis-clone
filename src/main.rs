92d7169869be78bbe7cb5497baa6ef2ad77b270c
killuazoldyck680 <killuaz2049@gmail.com>
2026-06-29 20:28:21 +0530

    add handle_conn function

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 
-use std::io::{Write,Read};
-use tokio::net::TcpListener;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+use tokio::net::{TcpListener, TcpStream};
+
+use  resp::Value;
+use anyhow::Result;
+
 mod resp;
+
 
 
 #[tokio::main]
@@ -21,17 +25,7 @@ async fn main() {
 
                 tokio::spawn(async move {
 
-                    let mut buf = [0;512];
-
-                    loop {
-                        let read_count = stream.read(&mut buf).await.unwrap();
-
-                        if read_count == 0 {
-                            break;
-                        }
-
-                        stream.write(b"+PONG\r\n").await.unwrap();
-                    }
+                    handle_conn(stream).await
                 });
                 
                 
@@ -40,6 +34,34 @@ async fn main() {
                 println!("error: {e}")
             }
         }
+    }
+}
+
+async fn handle_conn(stream: TcpStream) {
+    let mut handler = resp::RespHandler::new(stream);
+
+    println!("Starting read loop");
+
+    loop {
+        let value = handler.read_value().await.unwrap();
+
+        println!("Got value {:?}", value);
+
+        let response = if let Some(v) = value {
+            let (command, args) = extract_command(v).unwrap();
+            match command.as_str() {
+                "ping" => Value::SimpleString("PONG".to_string()),
+                "echo" => args.first().unwrap().clone(),
+                c => panic!("Cannot handle command {}", c),
+
+            }
+        } else {
+            break;
+        };
+
+        println!("Sending value {:?}", response);
+
+        handler.write_value(response).await.unwrap();
     }
 }
 
