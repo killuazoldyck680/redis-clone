97699211f52f5c6a46ea9de1120cb9b5e0d3768f
killuazoldyck680 <killuaz2049@gmail.com>
2026-06-29 19:34:44 +0530

    added remaining functions

--- src/resp.rs
+++ src/resp.rs
@@ -1,6 +1,6 @@
 use tokio::{net::TcpStream, io::{AsyncReadExt, AsyncWriteExt}};
 use bytes::BytesMut;
-use anyhow::Result;
+use anyhow:: Result;
 
 
 
@@ -41,7 +41,9 @@ impl RespHandler {
             return Ok(None)
         }
 
-        let (v, _) = parse_message(self.buffer.split())?;
+        let (v, bytes_consumed) = parse_message(self.buffer.clone())?;
+
+        let _ = self.buffer.split_to(bytes_consumed);
         Ok(Some(v))
 
 
@@ -52,7 +54,62 @@ impl RespHandler {
     
 }
 
-fn parse_message(buffer: BytesMut) -> Result<(Value,usize)>{}
+fn parse_message(buffer: BytesMut) -> Result<(Value,usize)>{
+    match buffer[0] as char {
+        '+' => parse_simple_string(buffer),
+        '*' => parse_array(buffer),
+        '$' => parse_bulk_string(buffer),
+        _ => Err(anyhow::anyhow!("Not a known value type {:?}", buffer)),
+
+    }
+}
+
+fn parse_simple_string(buffer: BytesMut) -> Result<(Value, usize)> {
+    if let Some((line, len)) = read_until_crlf(&buffer[1..]) {
+        let string = String::from_utf8(line.to_vec()).unwrap();
+
+        return Ok((Value::SimpleString(string), len + 1))
+    }
+
+    return Err(anyhow::anyhow!("Invalid string {:?}", buffer));
+}
+
+fn parse_array(buffer: BytesMut) -> Result<(Value, usize)> {
+    let (array_length, mut bytes_consumed) = if let Some((line, len)) = read_until_crlf(&buffer[1..]) {
+        let array_length = parse_int(line)?;
+
+        (array_length, len + 1)
+    } else {
+        return Err(anyhow::anyhow!("Invalid array format {:?}", buffer));
+    };
+
+    let mut items = vec![];
+
+    for _ in 0..array_length {
+        let (array_item, len) = parse_message(BytesMut::from(&buffer[bytes_consumed..]))?;
+
+        items.push(array_item);
+        bytes_consumed += len;
+    }
+
+    return Ok((Value::Array(items), bytes_consumed))
+}
+
+fn parse_bulk_string(buffer: BytesMut) -> Result<(Value, usize)> {
+    let (bulk_str_len, bytes_consumed) = if let Some((line, len)) = read_until_crlf(&buffer[1..]) {
+        let bulk_str_len = parse_int(line)?;
+
+        (bulk_str_len, len + 1)
+    } else {
+        return Err(anyhow::anyhow!("Invalid array format {:?}", buffer));
+    };
+
+    let end_of_bulk_str = bytes_consumed + bulk_str_len as usize;
+
+    let total_parsed = end_of_bulk_str + 2;
+
+    Ok((Value::BulkString(String::from_utf8(buffer[bytes_consumed..end_of_bulk_str].to_vec())?), total_parsed))
+}
 
 
 fn read_until_crlf(buffer: &[u8]) -> Option<(&[u8], usize)> {
