63c21967e2600821b9914612d5d39cf11ca8f7da
killuazoldyck680 <killuaz2049@gmail.com>
2026-07-06 19:54:00 +0530

    removed tests

--- src/main.rs
+++ src/main.rs
@@ -405,4 +405,4 @@ fn unpack_bulk_str(value: Value) -> Result<String> {
         Value::BulkString(s) => Ok(s),
         _ => Err(anyhow::anyhow!("Expected command to be a bulk string")),
     }
-}  kkk
+} 
