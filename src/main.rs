ac432daf233f245c1d0de4cd3d17f238c9db7940
killuazoldyck680 <killuaz2049@gmail.com>
2026-08-07 18:49:51 +0530

    defined master_replid and master_repl_offset

--- src/main.rs
+++ src/main.rs
@@ -85,6 +85,10 @@ async fn main() {
 }
 
 async fn execute_command(command: &str, args: Vec<Value>, db: &Db, is_replica: bool) -> Value {
+    let master_replid = "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb";
+
+    let master_repl_offset = 0;
+
     match command.to_lowercase().as_str() {
         "ping" => Value::SimpleString("PONG".to_string()),
                 "echo" => args.first().unwrap().clone(),
