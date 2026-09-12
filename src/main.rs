bd6b62d900f05bd6fd0872494594b495a32ab344
killuazoldyck680 <killuaz2049@gmail.com>
2026-09-12 20:42:35 +0530

    new section

--- src/main.rs
+++ src/main.rs
@@ -1743,7 +1743,7 @@ Value::SimpleString("OK".to_string())
         {
             let mut registry = sub_registry.lock().unwrap();
 
-            registry.entry(channel_name.clone())
+            registry.entry(channel_name.clone())kk
             .or_default()
             .push(tx.clone());
         }
