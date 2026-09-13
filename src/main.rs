32496db800e0731190d450f8242f1698857bb993
killuazoldyck680 <killuaz2049@gmail.com>
2026-09-13 19:50:53 +0530

    tx clone

--- src/main.rs
+++ src/main.rs
@@ -1745,7 +1745,7 @@ Value::SimpleString("OK".to_string())
 
             registry.entry(channel_name.clone())
             .or_default()
-            .push(tx.clone());
+            .push(tx.clone());tx
         }
 
         let response = Value::Array(vec![
